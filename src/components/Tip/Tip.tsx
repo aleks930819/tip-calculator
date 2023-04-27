e7ef622b166522093769bd1bb91b19ab96923962
Aleksandar Atanasov <aleks930819@gmail.com>
2023-04-27 12:54:57 +0300

    added number of people

--- src/components/Tip/Tip.tsx
+++ src/components/Tip/Tip.tsx
@@ -1,7 +1,44 @@
+import styles from './Tip.module.css';
 import React, { FC, ReactElement } from 'react';
 
+type ButtonProps = {
+  value: number;
+};
+
+const Button: FC<ButtonProps> = ({ value }): ReactElement => {
+  return <button>{value}$</button>;
+};
+
+const tipsChoices = [
+  {
+    value: 5,
+  },
+  {
+    value: 10,
+  },
+  {
+    value: 15,
+  },
+  {
+    value: 25,
+  },
+  {
+    value: 50,
+  },
+];
+
 const Tip: FC = (): ReactElement => {
-  return <div>TIP</div>;
+  return (
+    <div className={styles.tip_container}>
+      <h3>Select Tip %</h3>
+      <div className={styles.tips_container}>
+        {tipsChoices.map((tip) => (
+          <Button key={tip.value} value={tip.value} />
+        ))}
+        <input type="number" placeholder="Custom" />
+      </div>
+    </div>
+  );
 };
 
 export default Tip;
